a0adb19b85ddc8039418010fd68558d9bef91e3c
RAJsaharaj <rajsaha03583@gmail.com>
2020-12-24 14:19:32 +0530

    remove warnings

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import web from "../src/images/s1.jpg";
+// import web from "../src/images/s1.jpg";
 import { NavLink } from "react-router-dom";
 
 const Card = (props) => {
@@ -7,15 +7,14 @@ const Card = (props) => {
     <>
       <div className="col-md-4 col-12 mx-auto">
         <div className="card">
-          <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc} />
+          <img src={props.imgsrc} className="card-img-top" alt="alt text" />
           <div className="card-body">
             <h5 className="card-title font-weight-bold">{props.title}</h5>
             <p className="card-text">
              
             </p>
             <NavLink activeClassName="menu_active"
-                        className="nav-link"
-                        to="/about" className="btn btn-primary">
+                        to="/about" className="btn btn-primary nav-link">
               About
             </NavLink>
           </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import web from "../src/images/gym2.jpg";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 import Commom from "./Commom";
 
 const Home = () => {
